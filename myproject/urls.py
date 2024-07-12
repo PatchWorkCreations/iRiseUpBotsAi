c9ee029c6a572a54779ebdb46d65e2e3d85af1a4
Julia <officialpatchwork07@gmail.com>
2024-07-12 14:47:10 +0800

    change

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,12 @@
-# /Users/Julia/Downloads/braine-package/myproject/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),  # Route root URL to myapp
+    path('', include('myapp.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
