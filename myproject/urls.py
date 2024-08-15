1c89db8076971f5a85692df632b505b7bad9e3d4
Julia <officialpatchwork07@gmail.com>
2024-08-15 19:35:38 +0800

    retyr payment updates urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,10 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from myapp import views  
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('customadmin/', include('customadmin.urls')),
+    path('capture-paypal-payment/', views.capture_paypal_order, name='capture_paypal_order'),  # This is your new URL pattern
     path('', include('myapp.urls')),
     
 ]
