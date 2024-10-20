97eeb2f756b42484c24522248d38219101b9bafc
Julia <officialpatchwork07@gmail.com>
2024-10-21 05:34:40 +0800

    updated

---
+++ minify_css.py
@@ -0,0 +1,21 @@
+import css_html_js_minify
+
+# Minify style.css
+with open('/Users/Julia/Downloads/braine-package/myapp/static/myapp/css/style.css', 'r') as css_file:
+    css_content = css_file.read()
+
+minified_css = css_html_js_minify.css_minify(css_content)
+
+with open('/Users/Julia/Downloads/braine-package/myapp/static/myapp/css/style.min.css', 'w') as minified_file:
+    minified_file.write(minified_css)
+
+# Minify bootstrap.css
+with open('/Users/Julia/Downloads/braine-package/myapp/static/myapp/css/bootstrap.css', 'r') as css_file:
+    css_content = css_file.read()
+
+minified_bootstrap_css = css_html_js_minify.css_minify(css_content)
+
+with open('/Users/Julia/Downloads/braine-package/myapp/static/myapp/css/bootstrap.min.css', 'w') as minified_file:
+    minified_file.write(minified_bootstrap_css)
+
+print("CSS files have been minified!")
