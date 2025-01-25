6b41e1238f659a82f2252deab73bea2ec5250603
Julia <juliavictorio16@Gmail.com>
2025-01-25 23:37:52 +0800

    updated

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from myapp import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path(' ', views.personalized_plan, name='personalized_plan'),
+    path('', views.personalized_plan, name='personalized_plan'),
     path('sign_in', views.sign_in, name='sign_in'),
     path('sign_out/', views.sign_out, name='sign_out'),
 
